29f43a11741619e01ff622233ad1a812b8336385
MM Pvt Ltd <mehbubhussain243@gmail.com>
2025-01-18 17:07:08 +0530

    Update wholesaler-data.js

--- wholesaler-data.js
+++ wholesaler-data.js
@@ -2,4 +2,5 @@ const wholesalersData = [
     { name: "Wholesaler 1", phone: "1234567890", location: "Location 1", products: "rice" },
     { name: "Wholesaler 2", phone: "0987654321", location: "Location 2", products: "wheat" },
     { name: "Wholesaler 3", phone: "1122334455", location: "Location 3", products: "rice, wheat" },
+    { name: "mohsin asskisser", phone: "1122334455", location: "Location 4", products: "drugs,alcohol,cocaine" },
 ];
